ceb1b0622658205b96444d683409942de826a6ce
alexiscatmiao <alessiadicampi@yahoo.it>
2018-10-03 16:43:08 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://telegram-echo-bot-master.herokuapp.com/';
+$BOT_TOKEN = '663818320:AAE0nGlnEP3pXaE-XfztQ6I_WZHEQOmPyFs';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
